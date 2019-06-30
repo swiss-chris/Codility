fca4611e0bd32acf525ab250878e962e51d80a99
Chris Dickinson <chris.dickinson@web.de>
2019-06-30 21:34:25 +0100

    separating test from implementation

--- src/2-arrays/odd_occurrences_in_array/odd_occurrences_in_array.test.js
+++ src/2-arrays/odd_occurrences_in_array/odd_occurrences_in_array.test.js
@@ -1,20 +1,4 @@
-const solution = N => {
-  const numbers = new Map();
-
-  N.forEach(n => {
-    if (numbers.has(n)) {
-      numbers.set(n, numbers.get(n) + 1);
-    } else {
-      numbers.set(n, 1);
-    }
-  });
-
-  for (const [n, count] of numbers) {
-    if (count % 2 === 1) {
-      return n;
-    }
-  }
-};
+import { solution } from "./odd_occurrences_in_array";
 
 test("test array", () => {
   expect(solution([9, 3, 9, 3, 9, 7, 9])).toBe(7);
